97bece95c771f57e4d85245ecbeea055528a0b79
Vlad <vladgod@mail.ru>
2023-06-13 21:26:40 +0300

    Refactored the string length check functionto one line

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-const checksLengthString = function(string, maxLength) {
+const checksLengthString = (string, maxLength) => {
   if (string.length <= maxLength) {
     return true;
   } else {
@@ -9,7 +9,12 @@ const checksLengthString = function(string, maxLength) {
 // eslint-disable-next-line no-console
 console.log(checksLengthString('Проверка длины строки', 21));
 
-const checksPalindromString = function(string) {
+const checksLengthString2 = (string, maxLength) => string.length <= maxLength;
+
+// eslint-disable-next-line no-console
+console.log(checksLengthString2('Проверка длины строки', 22));
+
+const checksPalindromString = (string) => {
   let normalString = string;
   normalString = normalString.replaseAll(' ','');
   normalString = normalString.toLowerCase();
