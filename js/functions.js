f20d6c3154d52bdc6fb05f2e4e77869733dcc36e
Vlad <vladgod@mail.ru>
2023-06-14 15:31:07 +0300

    Fixes a bug in the palindrome function and adds the best option from live.

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,45 @@
-const checksLengthString = (string, maxLength) => {
+//Декларативно объявленная функция для проверки длины строки с классическим смнтаксисом
+function checksLengthString(string, maxLength) {
   if (string.length <= maxLength) {
     return true;
   } else {
     return false;
   }
-};
+}
 
-// eslint-disable-next-line no-console
-console.log(checksLengthString('Проверка длины строки', 21));
+checksLengthString('Проверка длины строки', 21);
 
+//стрелочная функция для проверки длины строки с сокращённым синтаксисом
 const checksLengthString2 = (string, maxLength) => string.length <= maxLength;
 
-// eslint-disable-next-line no-console
-console.log(checksLengthString2('Проверка длины строки', 22));
+checksLengthString2('Проверка длины строки 2', 23);
 
+
+//Функция для проверки, является ли строка палиндромом, решение в лоб по инструкции в задании
 const checksPalindromString = (string) => {
-  let normalString = string;
-  normalString = normalString.replaseAll(' ','');
-  normalString = normalString.toLowerCase();
-  let invertedString = '';
-  for(let i = normalString.length - 1; i === 0; i --) {
-    invertedString += normalString[i];
+  let normalizeString = string.replaceAll(' ', '');
+  normalizeString = normalizeString.toLowerCase();
+  let reverseString = '';
+  for(let i = normalizeString.length - 1; i >= 0; i --) {
+    reverseString += normalizeString[i];
   }
-  if (invertedString === normalString) {
+  if (reverseString === normalizeString) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
-// eslint-disable-next-line no-console
-console.log(checksPalindromString('Лёша на полке клопа нашёл'));
+checksPalindromString('Лёша на полке клопа нашёл');
+
+//Функция для проверки, является ли строка палиндромом, оптимальное решение по лайву
+const isPalindrom = (string) => {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < normalizeString.length / 2; i++) {
+    if (normalizeString[i] !== normalizeString[normalizeString.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isPalindrom('Топот');
