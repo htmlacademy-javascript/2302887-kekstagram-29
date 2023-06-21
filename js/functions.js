346e8d39d388d3aea232b3c9755df99347d1f7f2
Vlad <vladgod@mail.ru>
2023-06-21 11:01:12 +0300

    Corrects a typo

--- js/functions.js
+++ js/functions.js
@@ -15,7 +15,7 @@ const checksLengthString2 = (string, maxLength) => string.length <= maxLength;
 
 checksLengthString2('Проверка длины строки', 21);
 
-//Функция, объявленная через назначение переменной для проверки длины строки
+//Функция, объявленная через назначение переменной для проверки длины строк
 
 const checksLengthString3 = function(string, maxLength) {
   if (string.length <= maxLength) {
