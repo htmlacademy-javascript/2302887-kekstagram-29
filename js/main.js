de38e7c74dc68b5deac4332fc90370db2ca601a6
Vlad <vladgod@mail.ru>
2023-06-22 14:16:18 +0300

    Improves naming and syntax

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ const DESCRIPTIONS = [
   'Ночь, отель, фонарь и пальма, бессмысленный и яркий свет.',
   'Ням, ням and go go...',
   'Love and sunset - romantic!',
-  'Крабы, они такие крабы',
+  'Крабы, они такие крабы))',
   'Драйв! Круть! Огонь!',
   'Два бегемота из разных миров...'
 ];
@@ -60,29 +60,27 @@ const createIdGenerator = () => {
 
 const generateCommentId = createIdGenerator();
 
-const createMessage = () => Array.from(
-  { length: getRandomInteger(1, 2) },
-  () => getRandomArrayElement(COMMENT_LINES),
-).join(' ');
+const createMessage = () => Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(COMMENT_LINES)).join(' ');
 
 const createComment = () => ({
   id: generateCommentId(),
   avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
   message: createMessage(),
-  name: getRandomArrayElement(NAMES),
+  name: getRandomArrayElement(NAMES)
 });
 
 const createPicture = (index) => ({
   id: index,
   url: `photos/${index}.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
+  description: DESCRIPTIONS[index - 1],
   likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
-  comments: Array.from({ length: getRandomInteger(0, COMMENT_COUNT) }, createComment,),
+  comments: Array.from({length: getRandomInteger(0, COMMENT_COUNT)}, createComment)
 });
 
-const getPictures = () => Array.from(
-  { length: PICTURE_COUNT },
-  (_, pictureIndex) => createPicture(pictureIndex + 1),
+const getPhotos = () => Array.from(
+  {length: PICTURE_COUNT},).map(
+  (_item, pictureIndex) => createPicture(pictureIndex + 1),
 );
 
-getPictures();
+getPhotos();
+console.log(getPhotos());
