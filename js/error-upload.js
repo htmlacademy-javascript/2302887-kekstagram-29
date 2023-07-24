40b2bf0bb6cda97463232b1c2d4cf2ca10294b0e
Vlad <vladgod@mail.ru>
2023-07-24 21:08:11 +0300

    Adds a function to download data from the server

--- js/error-upload.js
+++ js/error-upload.js
@@ -1,7 +1,14 @@
+//Модуль формирования сообщения об ошибке загрузки данных с сервера
+
+//Определяем время показа сообщения
 const ALERT_SHOW_TIME = 7000;
 
+//Функция создания и стилизации сообщения
+
 const showAlert = (message) => {
+  //Создаём шаблон элемента сообщения об ошибке
   const alert = document.createElement('div');
+  //Стилизуем сообщение об ошибке
   alert.style.position = 'absolute';
   alert.style.left = '0';
   alert.style.top = '0';
@@ -10,9 +17,11 @@ const showAlert = (message) => {
   alert.style.fontSize = '20px';
   alert.style.textAlign = 'center';
   alert.style.background = '#df4115';
+  //Добавляем текст сообщения об ошибке
   alert.textContent = message;
+  //Добавляем шаблон в DOM
   document.body.append(alert);
-
+  //Используем встроенную функцию задержки удаления сообщения
   setTimeout(() => {
     alert.remove();
   }, ALERT_SHOW_TIME);
