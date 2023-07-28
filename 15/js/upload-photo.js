3b7099d394964503f38c64303438a6241d54ee86
Keks <keks@htmlacademy.ru>
2023-07-28 16:31:33 +0300

    :heavy_check_mark: Сборка #15

--- 15/js/upload-photo.js
+++ 15/js/upload-photo.js
@@ -9,15 +9,16 @@ const fileChooser = document.querySelector('.img-upload__input');
 const uploadPreview = document.querySelector('.img-upload__preview img');
 // Подписываем кнопку Загрузить на событие change
 fileChooser.addEventListener('change', () => {
-  // И в случае нажатия записываем первый выбранный файл
+  // И в случае нажатия записываем первый выбранный файл в виде объекта массива
   const file = fileChooser.files[0];
-  // Переводим имя загружаемого файла в нижний регистр
+  // Находим в объекте имя загружаемого файла и переводим его в нижний регистр для унификации
   const fileName = file.name.toLowerCase();
-  // Проверяем тип файла
+  // Проверяем тип выбранного файла через сравнение окончания имени с массивом допустимых окончаний, итерированного методом some
   const matches = FILE_TYPES.some((value) => fileName.endsWith(value));
   // Если тип верен
   if (matches) {
-    // Заменяем в DOM путь дефолтного файла для предварительного просмотра на путь к выбранному файлу
+    // Заменяем в DOM путь дефолтного файла для предварительного просмотра на путь к выбранному файлу методом createObjectURL,
+    // применённый к встроенному в браузер  объекту URL
     uploadPreview.src = URL.createObjectURL(file);
   }
 });
